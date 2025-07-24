1fbcf59719a77eb38f22cfe52c2392ebe3c617f7
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-07-24 17:48:13 +0545

    title cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-import "./titlecard.css";
+import React, { useEffect, useRef } from "react";
+import "./titlecards.css";
+import cards_data from "../../assets/cards/Cards_data";
+
 const TitleCards = () => {
-  return <div></div>;
+  const cardsRef = useRef();
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaV;
+  };
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+  return (
+    <div className="title-cards">
+      <h2>Popular on Netfilx</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
