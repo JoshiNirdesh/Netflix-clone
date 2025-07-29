560bf7b6073afda63486a05e230a4c58333f1671
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-07-29 12:54:38 +0545

    responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -30,7 +30,9 @@ const Home = () => {
               More Info
             </button>
           </div>
-          <TitleCards />
+          <div className="title-cards">
+            <TitleCards />
+          </div>
         </div>
       </div>
       <div className="more-cards">
